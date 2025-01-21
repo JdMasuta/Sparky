37f45d28e67afa335b13be80e9cfa8314412ce67
JdMasuta <jdmeesey@gmail.com>
2025-01-21 00:53:44 -0700

    NavBar centering and spacing

--- portable-app/frontend/src/pages/Home.jsx
+++ portable-app/frontend/src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import MainNavBar from "../components/shared/MainNavBar";
-import logo from "../assets/images/BW Integrated Systems.png";
 
 function Home() {
   return (
     <div className="container">
-      <div>
         <MainNavBar />
-      </div>
     </div>
   );
 }
