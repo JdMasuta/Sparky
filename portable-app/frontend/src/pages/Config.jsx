39cf66f12fff5cf485c92a6d799f56543d5fa9dd
JdMasuta <jdmeesey@gmail.com>
2025-01-22 16:04:36 -0700

    Removed vestigial banner from config.jsx

--- portable-app/frontend/src/pages/Config.jsx
+++ portable-app/frontend/src/pages/Config.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import MainNavBar from "../components/shared/MainNavBar.jsx";
-import logo from "../assets/images/BW Integrated Systems.png";
 import EmailReports from "../components/config/EmailReports.jsx";
 import DatabaseManagement from "../components/config/DatabaseManagement.jsx";
 import useTableData from "../components/config/useTableData.jsx";
@@ -14,11 +13,8 @@ function Config() {
 
   return (
     <div>
-      <div className="logo-container">
-        <img src={logo} alt="BW Integrated Systems" className="logo-image" />
-      </div>
       <MainNavBar />
-
+      
       <div className="container">
         <h1 className="config-title">Settings</h1>
 
